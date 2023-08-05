715e620008a6cf357af0502fff7058be55ecc662
Siddhant Dalvi <93437464+siddhant-dalvi@users.noreply.github.com>
2023-08-05 22:41:09 +0530

    Create Pattern 7.cpp

---
+++ Pattern 7.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // Space
+        
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j < i; j++) {
+                cout << " ";
+            }
+            for (int j = 1; j <= 2 * (n - i) + 1; j++) {
+                cout << "*";
+            }
+            cout << endl;
+        
+        }
+    }
+    
+    
+};
