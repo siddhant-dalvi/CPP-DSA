b1113aa410bc7d917fe37df5e26b7ac385c21d1e
Siddhant Dalvi <93437464+siddhant-dalvi@users.noreply.github.com>
2023-08-10 20:17:01 +0530

    Create Pattern 18.cpp

---
+++ Pattern 18.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    void printTriangle(int n) {
+          for(int i=0; i<n; i++){
+            char ch='A'+(n-1);
+            for(int j=0; j<=i; j++){
+                cout<<ch<<' ';
+                ch--;
+            }
+            cout<<endl;
+        }
+    }
+};
